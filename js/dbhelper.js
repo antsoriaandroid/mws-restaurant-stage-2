6ca4b837a5054dffec46104c10079be55edb7a93
Antonio Soria <antsoriaandroid@gmail.com>
2018-04-14 12:58:16 +0200

    fix: Fixed problem to load images on Github.io

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -156,7 +156,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
