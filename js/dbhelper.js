3a3b97edcd53c5ff45035b1441c171e371d1cfba
Antonio Soria <antsoriaandroid@gmail.com>
2018-07-05 23:46:16 +0200

    fix: URL address corrected to use the new server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -12,7 +12,7 @@ class DBHelper {
      */
     static get DATABASE_URL() {
         const port = 1337; // Change this to your server port
-        return `http://localhost:${port}/restaurants`;
+        return `http://localhost:${port}/restaurants/`;
     }
 
     /*Method to open an IDB*/
@@ -200,7 +200,7 @@ class DBHelper {
      * Restaurant page URL.
      */
     static urlForRestaurant(restaurant) {
-        return (`./restaurant.html?id=${restaurant.id}`);
+        return DBHelper.DATABASE_URL+restaurant.id;
     }
 
     /**
