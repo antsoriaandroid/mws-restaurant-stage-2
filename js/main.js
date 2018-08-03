8ac2c8c80f2e9f44789891aee8f416538a0986d4
Antonio S <antsoriaandroid@gmail.com>
2018-08-03 10:09:56 +0200

    feat: Added Mapbox maps as Google Maps is a paid service

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let restaurants,
     neighborhoods,
     cuisines;
-var map;
+var newMap;
 var markers = [];
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+	initMap();
     fetchNeighborhoods();
     fetchCuisines();
 });
@@ -68,10 +69,10 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize Google map, called from HTML.
+ * Initialize leaflet map, called from HTML.
  */
 window.initMap = () => {
-    let loc = {
+   /* let loc = {
         lat: 40.722216,
         lng: -73.987501
     };
@@ -80,7 +81,23 @@ window.initMap = () => {
         center: loc,
         scrollwheel: false
     });
-    updateRestaurants();
+    updateRestaurants();*/
+	initMap = () => {
+  self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
+        zoom: 12,
+        scrollWheelZoom: false
+      });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1IjoiYW50c29yaWFhbmRyb2lkIiwiYSI6ImNqa2RwN29tcjNhYXkzcXBhZWtjNWFhNXMifQ.YW_BB6EsuuEI9sOmZKt4vw',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
+  updateRestaurants();
+}
 }
 
 /**
@@ -200,14 +217,22 @@ createRestaurantHTML = (restaurant) => {
  * Add markers for current restaurants to the map.
  */
 addMarkersToMap = (restaurants = self.restaurants) => {
-    restaurants.forEach(restaurant => {
+    /*restaurants.forEach(restaurant => {
         // Add marker to the map
         const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
         google.maps.event.addListener(marker, 'click', () => {
             window.location.href = marker.url
         });
         self.markers.push(marker);
-    });
+    });*/
+	  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+    marker.on("click", onClick);
+    function onClick() {
+      window.location.href = marker.options.url;
+    }
+  });
 }
 
 if ('serviceWorker' in navigator) {
